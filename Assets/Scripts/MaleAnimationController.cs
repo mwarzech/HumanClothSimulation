2db3d2803d4cb0f30c17903264e6b81c4850ca12
Kamil Skomro <kamilskomrokamil@gmail.com>
2020-10-31 10:46:30 +0100

    add amination

---
+++ Assets/Scripts/MaleAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaleAnimationController : MonoBehaviour
+{
+
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            if (anim.GetBool("dancing"))
+            {
+                anim.SetBool("dancing", false);
+            }
+            else
+            {
+                anim.SetBool("dancing", true);
+            }
+        }
+    }
+}
