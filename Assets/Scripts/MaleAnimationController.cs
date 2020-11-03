3b9b3416199ad257e8dbd13bfb30bdc4574aa09c
NoOne <micwar@interia.pl>
2020-11-03 23:35:26 +0100

    Add animations dropdown menu and play button UI

--- Assets/Scripts/MaleAnimationController.cs
+++ Assets/Scripts/MaleAnimationController.cs
@@ -1,29 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using TMPro;
 
 public class MaleAnimationController : MonoBehaviour
 {
+    public string[] animations;
+
+    public TMP_Dropdown dropdown;
+    public TMP_Text buttonLabel;
 
     private Animator anim;
+
+    private const string idleStateName = "Idle";
+    private bool isPlaying = false;
+
+    public void AnimationButton()
+    {
+        if (!isPlaying)
+        {
+            PlayAnimation();
+        }
+        else
+        {
+            StopAnimation();
+        }
+    }
+
+    private void PlayAnimation()
+    {
+        anim.Play(dropdown.options[dropdown.value].text);
+        buttonLabel.text = "Stop Animation";
+        isPlaying = true;
+    }
+
+    private void StopAnimation()
+    {
+        anim.Play(idleStateName);
+        buttonLabel.text = "Play Animation";
+        isPlaying = false;
+    }
 
     void Start()
     {
         anim = GetComponent<Animator>();
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown("space"))
+        if(animations.Length > 0)
         {
-            if (anim.GetBool("dancing"))
-            {
-                anim.SetBool("dancing", false);
-            }
-            else
-            {
-                anim.SetBool("dancing", true);
-            }
+            dropdown.options.AddRange(animations.Select(x => new TMP_Dropdown.OptionData(x)));
+            dropdown.captionText.text = animations[0];
         }
     }
 }
