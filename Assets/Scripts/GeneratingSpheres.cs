ddda2242e14f185744ba87b36e3141be5611bb6c
NoOne <micwar@interia.pl>
2020-11-15 14:02:58 +0100

    Add sphere generator

---
+++ Assets/Scripts/GeneratingSpheres.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratingSpheres : MonoBehaviour
+{
+    public SkinnedMeshRenderer meshRenderer;
+    public Cloth cloth;
+    public float sphereRadius = 0.1f;
+    public int skipVertex = 0;
+
+    private GameObject[] spheres;
+    private Mesh mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = meshRenderer.sharedMesh;
+        CreateSpheres();
+    }
+
+    private void CreateSpheres()
+    {
+        spheres = new GameObject[(int)(mesh.vertices.Length / (1+skipVertex))];
+        cloth.sphereColliders = new ClothSphereColliderPair[spheres.Length];
+        for (int i = 0; i < spheres.Length; ++i)
+        {
+            spheres[i] = new GameObject("Sphere");
+            spheres[i].transform.position = mesh.vertices[i * (1 + skipVertex)];
+            SphereCollider collider = spheres[i].AddComponent<SphereCollider>();
+            collider.radius = sphereRadius;
+            cloth.sphereColliders[i] = new ClothSphereColliderPair(collider);
+        }
+    }
+
+    private void UpdateSpheres()
+    {
+        for (int i = 0; i < spheres.Length; ++i)
+        {
+            spheres[i].transform.position = mesh.vertices[i * (1 + skipVertex)];
+        }
+    }
+
+    void Update()
+    {
+        //UpdateSpheres();
+    }
+}
--- Assets/Scripts/MaleAnimationController.cs
+++ Assets/Scripts/MaleAnimationController.cs
@@ -8,12 +8,13 @@ public class MaleAnimationController : MonoBehaviour
 {
     public string[] animations;
 
+    public GameObject cloth;
     public TMP_Dropdown dropdown;
     public TMP_Text buttonLabel;
 
-    private Animator anim;
+    public Animator anim;
 
-    private const string idleStateName = "Idle";
+    private const string idleStateName = "IdlePose";
     private bool isPlaying = false;
 
     public void AnimationButton()
@@ -37,18 +38,33 @@ public class MaleAnimationController : MonoBehaviour
 
     private void StopAnimation()
     {
+        Destroy(cloth);
+        cloth = cloth_copy;
+        cloth.SetActive(true);
+        cloth_copy = Instantiate(cloth);
+        cloth_copy.SetActive(false);
+        //cloth.SetActive(false);
+        //cloth.SetActive(true);
         anim.Play(idleStateName);
         buttonLabel.text = "Play Animation";
         isPlaying = false;
     }
 
+    private GameObject cloth_copy;
+
     void Start()
     {
-        anim = GetComponent<Animator>();
+        cloth_copy = Instantiate(cloth);
+        cloth_copy.SetActive(false);
+        if (null == anim)
+        {
+            anim = transform.GetComponent<Animator>();
+        }
         if(animations.Length > 0)
         {
             dropdown.options.AddRange(animations.Select(x => new TMP_Dropdown.OptionData(x)));
             dropdown.captionText.text = animations[0];
+            dropdown.value = 0;
         }
     }
 }
